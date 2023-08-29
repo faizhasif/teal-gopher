6a250a48aa5137d8256cac72e724e5c2b4501e3a
Faiz Hasif <faiz@viprewards.com.my>
2023-08-29 16:25:26 +0800

    Add signup route

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"teal-gopher/controllers"
 	"teal-gopher/initializers"
 
 	"github.com/gin-gonic/gin"
@@ -15,10 +16,7 @@ func init() {
 func main() {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.POST("/signup", controllers.Signup)
+
+	r.Run()
 }
